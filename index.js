0bfe03387589566c089d0b490b7c51c8fd0e5fb5
Jacob Berman <jacobberman1995@gmail.com>
2018-03-27 18:45:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ function takeANumber(line, name){
 }
 
 function nowServing(){
-  var name = line[0];
+  if (katzDeliLine.length = 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+  var name = katzDeliLine[0];
+  katzDeliLine.shift();
+  
+  return name
+  }
+
   
 }
